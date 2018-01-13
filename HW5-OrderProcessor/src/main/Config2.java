f277b79ed039ffaf264e48b2f1cdc20529db7a4c
Enrique Nieto Arranz <nietoarranzmail@gmail.com>
2018-01-13 18:09:26 +0100

    Files added

---
+++ HW5-OrderProcessor/src/main/Config2.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Config2 {
+	 
+    // Defines the JNDI context factory.
+    public final static String JNDI_FACTORY = "weblogic.jndi.WLInitialContextFactory";
+ 
+    // Defines the JMS context factory.
+    public final static String JMS_FACTORY = "BookingsCF";
+ 
+    // URL
+    public final static String PROVIDER_URL = "t3://localhost:7001";
+ 
+}
---
+++ HW5-OrderProcessor/src/main/Config3.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Config3 {
+	 
+    // Defines the JNDI context factory.
+    public final static String JNDI_FACTORY = "weblogic.jndi.WLInitialContextFactory";
+ 
+    // Defines the JMS context factory.
+    public final static String JMS_FACTORY = "NewTripsCF";
+ 
+    // URL
+    public final static String PROVIDER_URL = "t3://localhost:7001";
+ 
+}
